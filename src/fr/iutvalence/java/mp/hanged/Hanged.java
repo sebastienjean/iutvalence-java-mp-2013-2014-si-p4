7ac18451874d37780f5d3e6fc18bfdb5a80c3edd
loicpascal <loicpascal@yahoo.fr>
2013-09-20 10:00:01 +0200

    Add class Hanged
    class which is the second main class named "pendu" which represent the
    game

---
+++ src/fr/iutvalence/java/mp/hanged/Hanged.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.java.mp.hanged;
+/**
+ * class which is the second main class named "pendu" which represent the game
+ * @author pascallo
+ *
+ */
+public class Hanged
+{
+    /**
+     * Game termination status
+     */
+    public boolean finishedHanged;
+}

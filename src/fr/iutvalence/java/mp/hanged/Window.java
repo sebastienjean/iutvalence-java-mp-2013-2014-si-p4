1bd0bea144cdd1d000a0b00e34e7530b87e750a8
loicpascal <loicpascal@yahoo.fr>
2013-12-12 21:02:20 +0100

    Update Window.java

--- src/fr/iutvalence/java/mp/hanged/Window.java
+++ src/fr/iutvalence/java/mp/hanged/Window.java
@@ -1,44 +1,37 @@
 package fr.iutvalence.java.mp.hanged;
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.JButton;
+
 
 /**
  * @author pascallo
  *
  */
 public class Window extends JFrame {
-    
-    
-    private JPanel pan = new JPanel();
-    
-    private JButton bouton = new JButton("Nouvelle Partie !");
-    /**
-     * Constructor
-     */
-    public Window(){
-        //
-        this.setTitle("Le pendu !");
-        this.setSize(800, 500);
-        this.setLocationRelativeTo(null);
-        this.setLocation(20, 80);
-        this.setResizable(false);
-        //this.setAlwaysOnTop(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-        this.setVisible(true);
-        //
-        pan.setBackground(Color.pink);
-        this.setContentPane(pan);               
-        this.setVisible(true);
-        //
-        
-        pan.add(bouton);
-        
-    }
-    
-    public void paintComponent(Graphics g){
-        g.drawString("Bonjour et bienvenu sur le jeu du pendu !", 10, 20);
-      } 
+
+
+	private final JButton bouton = new JButton("Nouvelle Partie !");
+	/**
+	 * Constructor
+	 */
+	public Window(){
+		//
+		this.setVisible(true);
+		this.setTitle("Le jeu du pendu !");
+		this.setSize(1000, 700);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+		//this.setResizable(false);
+
+		//this.setAlwaysOnTop(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Jpanel
+		JPanel pan = new JPanel();
+		pan.setBackground(Color.pink);
+		this.setContentPane(new Panel());
+	}
 }

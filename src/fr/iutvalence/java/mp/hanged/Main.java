092b824c6e5b9d28944df0a1636f82aa2a9519cc
loicpascal <loicpascal@yahoo.fr>
2013-12-12 20:55:07 +0100

    Update Main.java

--- src/fr/iutvalence/java/mp/hanged/Main.java
+++ src/fr/iutvalence/java/mp/hanged/Main.java
@@ -2,17 +2,18 @@ package fr.iutvalence.java.mp.hanged;
 
 /**
  * This is the main class which begin the game
- * @author pascallo 
+ * @author pascallo
  */
 public class Main
 {
-    /**
-     * This is the main which begin the game
-     * @param args
-     * 
-     */
-    public static void main(String[] args)
-    {
-        new Hanged().play();
-    }
+	/**
+	 * This is the main which begin the game
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args)
+	{
+		//new Window();
+		new Hanged().play();
+	}
 }

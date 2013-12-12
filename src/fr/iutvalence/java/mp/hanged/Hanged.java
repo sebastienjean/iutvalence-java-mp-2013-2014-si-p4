968b52bf6be7d325ff62c9c028a1bcae58fa149e
loicpascal <loicpascal@yahoo.fr>
2013-12-12 20:57:41 +0100

    Update Hanged.java

--- src/fr/iutvalence/java/mp/hanged/Hanged.java
+++ src/fr/iutvalence/java/mp/hanged/Hanged.java
@@ -1,6 +1,5 @@
 package fr.iutvalence.java.mp.hanged;
 
-
 /**
  * Represents a "Hanged" game
  * 
@@ -9,124 +8,124 @@ package fr.iutvalence.java.mp.hanged;
  */
 public class Hanged
 {
-    /**
-     * score of the player
-     */
-    private int score;
+	/*-------------------------------------------------------------
+	 *  ATRIBUTES
+	 *-----------------------------------------------------------*/
 
-    /**
-     * boolean which indicates if the game is finished
-     */
-    // TODO (fix) this fields should be declared private
-    private boolean finishedHanged;
+	/**
+	 * score of the player
+	 */
+	private int score = 50;
 
-    /**
-     * This is the hidden word
-     */
-    // TODO (fix) this fields should be declared private
-    private char hiddenWord[];
+	/**
+	 * boolean which indicates if the game is finished
+	 */
+	// TODO (fix) this fields should be declared private
+	private boolean finishedHanged;
 
-    /**
-     * This is the number of error
-     */
-    // TODO (fix) this fields should be declared private
-    private int errorNumber;
+	/**
+	 * This is the hidden word
+	 */
+	// TODO (fix) this fields should be declared private
+	private char hiddenWord[];
 
-    /**
-     * word to find
-     */
-    // TODO (fix) this fields should be declared private
-    private String wordToFind;
+	/**
+	 * word to find
+	 */
+	// TODO (fix) this fields should be declared private
+	private final String wordToFind;
 
-    private final static int nbErrorMax = 5;
+	private final static int nbErrorMax = 6;
 
-    /**
-     * Initialize : - begunHanged; - finishedHanged; - the score - the player's
-     * alias - the wordToFind
-     */
-    public Hanged()
-    {
-        // Creation of the player
-        new Display().displayPlayer();
+	/*-------------------------------------------------------------
+	 *  CONSTRUCTORS
+	 *-----------------------------------------------------------*/
 
-
-        // Initialization of the wordToFind
-        System.out.println("   Recherche du mot...");
-        this.wordToFind =  new WordLibrary().getWordToFind();
-        System.out.println("\t --> Le mot à trouver est : TROLOLOLOLOLOOOLOL" );
-        
-        //Window fen = new Window();
-    }
-    // TODO (fix) rewrite comment (this is not a constructor)
-    /**
-     * Method which engage the game
-     */
-    public void play()
-    {
-        boolean finishedHanged = false;
-        char letter;
-        int errorNumber = 0;
-        boolean typedLetter = false;
-        // Initialization of the secret word
-        char[] hiddenWord = new char[this.wordToFind.length()];
+	/**
+	 * Initialize : - finishedHanged; - the score - the player's
+	 * alias - the wordToFind
+	 */
+	public Hanged()
+	{
+		// Creation of the player
+		new GetAndDisplay().displayPlayer();
 
 
-        //filling and displaying of the hiddenWord
-        //WordLibrary.giveHiddenWord(this.wordToFind);
-        
-        
-        System.out.print("Le mot caché est : ");
+		// Initialization of the wordToFind
+		this.wordToFind =  new WordLibrary().getWordToFind();
 
-        for (int i = 0; i < this.wordToFind.length(); i++)
-        {
-            hiddenWord[i] = '_';
-            System.out.print(hiddenWord[i] + " ");
-        }
-        System.out.println("");
+		//Window fen = new Window();
+	}
+	// TODO (fix) rewrite comment (this is not a constructor)
+	/**
+	 * Method which engage the game
+	 */
+	public void play()
+	{
+		boolean finishedHanged = false;
+		char letter;
+		int errorNumber = 0;
+		boolean typedLetter = false;
+
+		// Initialization of the secret word
+		this.hiddenWord = new char[this.wordToFind.length()];
 
 
+		//filling and displaying of the hiddenWord
+		new GetAndDisplay().displayHiddenWord(this.wordToFind, this.hiddenWord);
 
-        while(finishedHanged == false){
+		while(finishedHanged == false){
 
-            letter =  new Display().giveLetter();
+			letter =  new GetAndDisplay().giveLetter();
 
-            System.out.println("La lettre proposee est : "+letter);
-            for (int i = 0; i < this.wordToFind.length(); i++)
-            {
-                if (letter == this.wordToFind.charAt(i))
-                {
-                    typedLetter = true;
-                    hiddenWord[i] = letter; 
-                }
+			System.out.println("La lettre proposée est : "+letter);
+			for (int i = 0; i < this.wordToFind.length(); i++)
+			{
+				if (letter == this.wordToFind.charAt(i))
+				{
+					typedLetter = true;
+					this.hiddenWord[i] = letter;
+				}
 
-            }
+			}
 
-            if (typedLetter == false) {
-                errorNumber++;
-                System.out.println("\t\t\t\tVous avez fait " + errorNumber + " erreurs");
-                //System.out.println(hiddenWord[i]);
-                if (errorNumber >= nbErrorMax) {
-                    System.out.println( "Vous avez perdu ! Le mot à trouver était: " + this.wordToFind); // Le joueur a perdu, on lui donne le mot qu'il devait deviner
-                    return;
-                }
-            }
-            for (int l = 0; l < hiddenWord.length; l++)
-            {
-                System.out.print(hiddenWord[l] + " ");
-            }
-            System.out.println();
-            typedLetter = false;
-            this.finishedHanged = true;
-            for( int j = 0; j< hiddenWord.length ; j++){
-                if (hiddenWord[j] == '_') {
-                    this.finishedHanged = false;
-                }
-            }
-            if(this.finishedHanged==true){
-                System.out.println();
-                System.out.println("Vous avez gagné !");
-                break;
-            }
-        }
-    }    
+			if (typedLetter == false) {
+				errorNumber++;
+				if (errorNumber<2)
+					System.out.println("\t\t\t\tNombre d'erreur = " + errorNumber);
+				else
+					System.out.println("\t\t\t\tNombre d'erreurs = " + errorNumber);
+				if (errorNumber >= nbErrorMax) {
+					this.score = 0;
+					new GetAndDisplay().GameLost(this.wordToFind, this.score);
+					new GetAndDisplay().newGame();
+					break;
+				}
+			}
+
+			for (char element : this.hiddenWord) {
+				System.out.print(element + " ");
+			}
+			System.out.println();
+			typedLetter = false;
+			this.finishedHanged = true;
+			for (char element : this.hiddenWord) {
+				if (element == '_') {
+					this.finishedHanged = false;
+				}
+			}
+			if(this.finishedHanged==true){
+				System.out.println();
+				System.out.print("Vous avez gagné ! ");
+				new Score().Score(errorNumber, this.score);
+				break;
+			}
+		}
+		new GetAndDisplay().newGame();
+	}
 }
+
+/**
+ * - Ecrire les règles et les afficher en début de partie !
+ *
+ */
